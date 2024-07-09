f4487931b46f3c36b3ab372ec23fa60d5ec2c4cd
Rashmina Amarasinghe <shasheenrashmina@gmail.com>
2024-07-09 19:14:32 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,64 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.red,
+      appBar: AppBar(
+        title: const Center(
+          child: Text(
+            'Dice',
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ),
+        backgroundColor: Colors.red,
+      ),
+      body: const DicePage(),
+    ),
+  ));
+}
+
+class DicePage extends StatefulWidget {
+  const DicePage({super.key});
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDice = 1;
+  int rightDice = 1;
+  void changedice() {
+    setState(() {
+      leftDice = Random().nextInt(6) + 1;
+      rightDice = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+                onPressed: () {
+                  changedice();
+                },
+                child: Image.asset('images/dice$leftDice.png')),
+          ),
+          Expanded(
+            child: TextButton(
+                onPressed: () {
+                  changedice();
+                },
+                child: Image.asset('images/dice$rightDice.png')),
+          ),
+        ],
+      ),
+    );
+  }
+}
